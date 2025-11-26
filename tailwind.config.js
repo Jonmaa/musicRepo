c16146bf9c740e97417cc752ea2f9ed0e1c3cecc
Jon Marcos Mercadé <jonmarcos1@gmail.com>
2025-11-26 19:46:14 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Circular Spotify Text"', 'system-ui', 'sans-serif'],
+        sans: ['InterVariable', 'Inter', 'system-ui', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        black: 900,
       },
       colors: {
         spotify: {
           green: "#1ed760",
+          black: "#000000",
           dark: "#121212",
           gray: "#181818",
           lightgray: "#282828",
         },
-      },
-      animation: {
-        "pulse-slow": "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
     },
   },
